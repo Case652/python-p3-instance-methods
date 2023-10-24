e912402eb1bc6843c47be556069433a635c86b31
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-10-24 14:35:41 -0500

    Completed

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,14 @@
 
 class Dog:
     # Class body goes here
-
+    
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
+        pass
     pass
+fido = Dog()
+fido.bark()
+fido.sit()
